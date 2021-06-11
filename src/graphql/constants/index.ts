94aab67b718db05f869f927fd950be977fbaaa52
steven-mathew <steven.mathew@mail.utoronto.ca>
2021-06-11 19:48:44 -0400

    remove uneeded files

--- src/graphql/constants/index.ts
+++
@@ -1,6 +0,0 @@
-import { baseUrl } from '~/config/seo'
-
-export const CLIENT_URL =
-  process.env.NODE_ENV === 'production' ? baseUrl : 'http://localhost:3000'
-
-export const PAGINATION_AMOUNT = 16
--- src/graphql/schema/index.ts
+++
@@ -1,6 +0,0 @@
-import { makeExecutableSchema } from 'graphql-tools'
-import typeDefs from '../typeDefs'
-import resolvers from '../resolvers'
-
-export const schema = makeExecutableSchema({ typeDefs, resolvers })
-export default schema
