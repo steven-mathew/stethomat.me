5db40c9d1f956fc5ce102140f0f863d2fae4b42a
steven-mathew <steven.mathew@mail.utoronto.ca>
2021-06-25 18:17:36 -0400

    feat: add frontMatter to blog

--- src/components/Layouts/index.tsx
+++ src/components/Layouts/index.tsx
@@ -1,5 +1,9 @@
 import {useRouter} from 'next/router'
+import Image from 'next/image'
+
 import * as React from 'react'
+
+import Page from '../Page'
 
 export function CenteredColumn({children}) {
     return (
@@ -11,12 +15,32 @@ export function BlogLayout({children, frontMatter}) {
     const router = useRouter()
     const slug = router.asPath.replace('/blog', '')
 
+
+    // const imageProps = {
+    //     src: frontMatter.image,
+    //     alt: frontMatter.alt,
+    // }
+
     return (
-        <>
+        <Page>
             <CenteredColumn>
+                <div className="space-y-12 md:space-y-16">
+                    <div className="flex-col self-start space-y-8 md:items-center flex-start">
+
+                        <div className="space-y-8">
+                            <h4 className="font-list-heading">{frontMatter.title}</h4>
+                        </div>
+
+                        <div className="content-between self-start self-center space-y-8">
 
 
+
+                        </div>
+
+                    </div>
+                    {children}
+                </div>
             </CenteredColumn>
-        </>
+        </Page>
     )
 }
