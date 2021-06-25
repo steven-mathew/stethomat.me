e9d659b2c00a4f1cc1791abf90d4389b01aeff9d
steven-mathew <steven.mathew@mail.utoronto.ca>
2021-06-25 17:46:15 -0400

    feat: blog layout

--- src/components/Layouts/index.tsx
+++ src/components/Layouts/index.tsx
@@ -1,9 +1,22 @@
-// https://github.com/brianlovin/brian-lovin-next/tree/main/src/components
-
+import {useRouter} from 'next/router'
 import * as React from 'react'
 
 export function CenteredColumn({children}) {
     return (
         <div className="mx-auto max-w-screen-sm justify-content">{children}</div>
+    )
+}
+
+export function BlogLayout({children, frontMatter}) {
+    const router = useRouter()
+    const slug = router.asPath.replace('/blog', '')
+
+    return (
+        <>
+            <CenteredColumn>
+
+
+            </CenteredColumn>
+        </>
     )
 }
